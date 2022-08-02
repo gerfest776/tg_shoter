dd742eae9d3b2279999f4e44b69b26f6d0563f88
Герман Гриценко <gerfest.gricenko@gmail.com>
2022-08-02 21:34:02 +0300

    feat: screenshot worker for users

--- handlers/users/screen.py
+++ handlers/users/screen.py
@@ -1,11 +1,10 @@
-import aiofiles
 import validators
 from aiogram import types
-from aiogram.types import ParseMode
+from aiogram.types import ParseMode, InputMedia, InputFile
 from aiogram.types.message import ContentType
 
 
-from core.loader import dp, bot
+from core.loader import dp
 from utils.screenshot_page import Screener
 
 
@@ -14,5 +13,8 @@ async def link_message(msg: types.Message):
     if not validators.url(msg.text):
         await msg.reply("Please, send correct url", parse_mode=ParseMode.MARKDOWN)
     else:
-        # mess = await msg.answer_photo(types.InputFile("./media/1000-7.jpeg"), "Your image is processing...")
-        await msg.answer_photo(types.InputFile(await Screener(msg.text).screen_page()))
+        mess = await msg.answer_photo(types.InputFile("./media/1x1.png"), "Your image is processing...")
+        file = InputMedia(media=InputFile(
+            await Screener(msg.text).screen_page()
+        ), caption="Your screenshot!")
+        await mess.edit_media(file)
