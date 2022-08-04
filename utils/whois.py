8905e9f89c0a7233c4ae7384100cd27b5e910c80
Герман Гриценко <gerfest.gricenko@gmail.com>
2022-08-04 07:21:03 +0300

    fix: docker

--- utils/whois.py
+++ utils/whois.py
@@ -8,8 +8,12 @@ from core.db.db_api import whois_table
 
 async def get_whois(url: str) -> None:
     logger.info(f"Start getting whois for {url}")
-    # ip = socket.gethostbyname(url)
-    ip = "108.177.16.0"
+
+    try:
+        ip = socket.gethostbyname(url)
+    except Exception as e:
+        logger.error(f"Failing getting ip with socket: {e}")
+
     res = await asyncwhois.aio_whois_ipv4(ip)
     try:
         await whois_table.insert_url_whois_to_db(
