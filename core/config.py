428ccc4e2cddec58b9346b8e3957b7270e37644f
Герман Гриценко <gerfest.gricenko@gmail.com>
2022-08-07 18:07:42 +0300

    feat: pytest

--- core/config.py
+++ core/config.py
@@ -6,11 +6,11 @@ class Settings:
     API_TOKEN = decouple.config("API_TOKEN", "test")
     ADMIN = decouple.config("ADMIN", "test")
 
-    PG_NAME = decouple.config("PG_NAME")
-    APG_HOST = decouple.config("PG_HOST")
-    PG_PORT = decouple.config("PG_PORT")
-    APG_USER = decouple.config("PG_USER")
-    APG_PASSWORD = decouple.config("PG_PASSWORD")
+    PG_NAME = decouple.config("PG_NAME", "test")
+    APG_HOST = decouple.config("PG_HOST", "test")
+    PG_PORT = decouple.config("PG_PORT", "test")
+    APG_USER = decouple.config("PG_USER", "test")
+    APG_PASSWORD = decouple.config("PG_PASSWORD", "test")
 
     @classmethod
     def get_db_connection_data(cls):
