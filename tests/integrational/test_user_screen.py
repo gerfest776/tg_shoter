61fbcd4a7fbf475a6425df6147575661d6503d02
Герман Гриценко <gerfest.gricenko@gmail.com>
2022-08-16 00:39:17 +0300

    sdelal izmenenia i tochka

--- tests/integrational/test_user_screen.py
+++ tests/integrational/test_user_screen.py
@@ -5,11 +5,13 @@ import pytest
 from aiogram.types import InputFile, InputMedia
 
 from core.db.db_api import WhoisAPI
+from core.db.tables import DatabaseTables
 from handlers.groups.screen_groups import handle_link_message_groups
 
 
 @pytest.mark.asyncio
 async def test_group_screen_handle():
+    await DatabaseTables().init_database()
     url = "https://github.com"
     message_mock = AsyncMock(text=f"/screen {url}")
     await handle_link_message_groups(message_mock)
